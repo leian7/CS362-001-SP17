e905b3910779292e406dd8ca0cf7f92b3711e422
leian7 <leian@osuosl.org>
2017-05-30 21:18:02 -0700

    clean up

--- projects/leian/assignment-4/Calendar/src/test/java/edu/osu/cs362/ApptRandomTest.java
+++ projects/leian/assignment-4/Calendar/src/test/java/edu/osu/cs362/ApptRandomTest.java
@@ -8,27 +8,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-
-/*	2. Identify all the dependencies (parameters)
-	3. Identify All methods, constructors, fields: isValid, setDescription
-	5. Write code to generate random inputs for the constructors
-	6. Write code to randomly choose a method of the CUT: either isValid or setDescription
-	7. Write code to generate random inputs for the chosen
-method: "if isValid, these kinds of inputs. else if setDescription, these kinds of inputs..."
-8. Invoke the method (execute the method)
-9. Check if stopping criterion is not satisfied go to step 4.
-● Note: steps 5 and 7:
-● If the input is a primitive data type,
-● generate a random primitive value
-● If the input is a reference, choose randomly among:
-● The null value
-● The constructor with no arguments (if it exists) */
-
-/* Constructor params:
- * int startHour, int startMinute, int startDay, int startMonth, int startYear, String title, String description. Just ints and strings */
-/* isValid params: None, but always called by constructor */
-/* setDescription params: String description */
-
 /**
  * Random Test Generator  for Appt class.
  */
